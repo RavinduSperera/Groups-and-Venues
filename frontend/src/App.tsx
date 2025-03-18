20958c4133009ccaef276c0db39fed82a6a8a9a6
RavinduPerera <it22064622@my.sliit.lk>
2025-03-19 02:33:26 +0530

    added venue page but something went wrong so need to check again

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/sidebar";
 import Navbar from "./components/navbar";
 import GroupManagement from "./pages/groupManagement";
+import VenueManagement from "./pages/venueManagement";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <div style={{ marginLeft: 240, padding: 20 }}>
           <Routes>
             <Route path="/groups" element={<GroupManagement />} />
+            <Route path="/venues" element={<VenueManagement />} /> 
             {/* Add other pages here */}
           </Routes>
         </div>
