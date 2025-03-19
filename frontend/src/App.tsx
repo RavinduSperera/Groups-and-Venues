829bba7f3e00467bc75cfc4c6e580fab1a3c1750
RavinduPerera <it22064622@my.sliit.lk>
2025-03-20 00:24:46 +0530

    added groups and venues routes to check the ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import GroupReport from "@/components/reports/GroupReport";
 import VenueReport from "@/components/reports/VenueReport";
+import GroupManagement from "@/pages/GroupManagement";
+import VenueManagement from "@/pages/VenueManagement";
 
 const App = () => {
   return (
@@ -8,6 +10,8 @@ const App = () => {
       <Routes>
         <Route path="/reports/groups" element={<GroupReport />} />
         <Route path="/reports/venues" element={<VenueReport />} />
+        <Route path="/pages/groups" element={<GroupManagement/>}/>
+        <Route path="/pages/venues" element={<VenueManagement/>}/>
       </Routes>
     </Router>
   );
